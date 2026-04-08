e36eb9421a9a669dc9fa849d90aa94097f2ed528
GirishaPriyadharsini <girisha2536@gmail.com>
2026-04-08 21:15:30 +0530

    fix port binding for railway

--- server.js
+++ server.js
@@ -489,6 +489,6 @@ app.use((err, req, res, next) => {
     next();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
